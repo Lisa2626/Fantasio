6c0c1b3b8c23aabfa774e969ba9c2a1f6d583278
Lisa2626 <lisa.drouglazet@gmail.com>
2025-01-24 15:28:44 +0100

    Sauvegarde des fichiers avant de faire un pull

---
+++ conf.py
@@ -0,0 +1,30 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Fantasio'
+copyright = '2025, Lisa Drouglazet'
+author = 'Lisa Drouglazet'
+release = '1.0'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon']  # Ajouter ces extensions
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'  # Modifier pour utiliser le thème sphinx_rtd_theme
+html_static_path = ['_static']
+
+html_output = './docs'
+
